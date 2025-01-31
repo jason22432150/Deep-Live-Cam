1fa2dd119733d806c80261cbb05c19bff90dc3e7
Jhen home <nmsl22432150@gmail.com>
2025-01-31 19:21:42 +0800

    Rename start.spec to Deep-Live-Cam.spec, add hidden import for expecttest, and create get_modules_file.py to list and format file paths; add modules.txt and run.spec for project structure and dependencies.

---
+++ get_modules_file.py
@@ -0,0 +1,18 @@
+import os
+
+def list_files_in_directory(directory):
+    file_paths = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_paths.append(f"'{os.path.abspath(os.path.join(root, file))}',")
+    return file_paths
+
+def replace_backslashes(file_paths):
+    return [file_path.replace('\\', '\\\\') for file_path in file_paths]
+
+if __name__ == "__main__":
+    directory = 'media'
+    files = list_files_in_directory(directory)
+    replaced_files = replace_backslashes(files)
+    for file in files:
+        print(file)
