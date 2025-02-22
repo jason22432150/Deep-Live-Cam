86df20672bcc7aaab86dafe6695cc6256b1e477f
Guo <nmsl22432150@gmail.com>
2025-02-23 02:27:32 +0800

    🛠️ chore: 更新相依並精簡 run.py

--- run.py
+++ run.py
@@ -5,15 +5,9 @@ import wmi
 
 if __name__ == "__main__":
 
-    m_wmi = wmi.WMI()
-    cpu_info = m_wmi.Win32_Processor()
-    if len(cpu_info) > 0:
-        serial_number = cpu_info[0].ProcessorId
-        print("cpu: ", serial_number)
-
     try:
         core.run(lang="zh-TW", CpuOrCuda="cuda")
     except Exception as e:
         print(e)
         input("Press Enter to exit...")
-    # core.run(lang="zh-TW", CpuOrCuda="cuda")
+
